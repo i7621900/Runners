ecaef352070696ebb41ed00730f67d67b3273877
i7621900 <i7621900@bournemouth.ac.uk>
2016-01-30 17:32:56 +0000

    Create Global.h

---
+++ Global.h
@@ -0,0 +1,9 @@
+#ifndef _GLOBAL_H_INCLUDED_
+#define _GLOBAL_H_INCLUDED_
+
+#define ScreenHeight 600 //Define set number for ScreenHeight
+#define ScreenWidth 800 //Define set number for ScreenWidth
+#define Left_Click mouse_b & 1 // Set left click as Left_Click
+#define ScreenEnd 10000 // End of the level
+
+#endif 
